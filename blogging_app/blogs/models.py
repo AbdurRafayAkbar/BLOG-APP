04d66eef78c8b6a9513c1251299e4185839a3a88
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-06-29 12:37:39 +0500

    Updated Models. Add Status field as well as order and index for displaying publish date

--- blogging_app/blogs/models.py
+++ blogging_app/blogs/models.py
@@ -2,12 +2,19 @@ from django.db import models
 from django.utils import timezone
 
 class Blogs(models.Model):
+    class status(models.TextChoices):
+        DRAFT="DR","Draft"
+        PUBLISHED="PB","published"
     blog_title=models.CharField(max_length=100)
     blog_description=models.TextField()
     slug=models.SlugField(unique=True,max_length=250)
     publish=models.DateTimeField(default=timezone.now)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateField(auto_now=True)
+    Status=models.CharField(max_length=10,choices=status,default=status.DRAFT)
 
+    class Meta():
+        ordering=['-publish']
+        indexes=[models.Index(fields=["-publish"])]
     def __str__(self):
         return self.blog_title
