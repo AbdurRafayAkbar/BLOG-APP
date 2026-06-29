9429d53376f0780be6d1b97f4782739a6148706c
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-06-29 12:30:16 +0500

    Created models and updated the settings

--- blogging_app/blogs/models.py
+++ blogging_app/blogs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Blogs(models.Model):
+    blog_title=models.CharField(max_length=100)
+    blog_description=models.TextField()
+    slug=models.SlugField(unique=True,max_length=250)
+    publish=models.DateTimeField(default=timezone.now)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.blog_title
